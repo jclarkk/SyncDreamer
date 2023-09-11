67840279438aecd52d908df74988aee399413f9d
Jonathan Clark <jonathan@hexa3d.io>
2023-09-11 11:56:29 +0300

    Use GLB and not PLY

--- train_renderer.py
+++ train_renderer.py
@@ -114,7 +114,7 @@ def extract_mesh(model, output, resolution=512):
 
     # output geometry
     mesh = trimesh.Trimesh(vertices, triangles, vertex_colors=vertex_colors)
-    mesh.export(str(f'{output}/mesh.ply'))
+    mesh.export(str(f'{output}/mesh.glb'))
 
 def main():
     parser = argparse.ArgumentParser()
