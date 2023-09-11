1e02229c5e66af2d51598022e55a1162d458de6b
Jonathan Clark <jonathan@hexa3d.io>
2023-09-11 15:06:41 +0300

    Reduce to 50k tri's and revert to PLY

--- train_renderer.py
+++ train_renderer.py
@@ -114,7 +114,8 @@ def extract_mesh(model, output, resolution=512):
 
     # output geometry
     mesh = trimesh.Trimesh(vertices, triangles, vertex_colors=vertex_colors)
-    mesh.export(str(f'{output}/mesh.glb'))
+    mesh.simplify_quadric_decimation(face_count=50000)
+    mesh.export(str(f'{output}/mesh.ply'))
 
 def main():
     parser = argparse.ArgumentParser()
